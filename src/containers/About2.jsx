7208f02bf1f202a674638336c9031285238c4d33
Dharshini <dharshini.k2022cce@sece.ac.in>
2025-01-23 01:12:43 +0530

    updated img

--- src/containers/About2.jsx
+++ src/containers/About2.jsx
@@ -5,7 +5,7 @@ const About2 = () => {
     return(
         <div className='main__div'>
             <div className='main__div__bg'>
-                <div className='main__img__div'></div>
+                <div className='main__img__div'> <img src = "blazer_pic.png" alt = "Blazer image"/></div>
                 <div className='main__text__div'>
                     <p>Hello!<span className='hand__movement'>👋</span> This is</p>
                    <b> <div className='name__display'>DHARSHINI KARTHIK</div> </b>
